d1676bd0b448cf37e6b3e26c3750f7ca8a7c5807
AnnaLizzy <thuthuha29@gmail.com>
2023-04-03 19:06:47 +0700

    add

---
+++ assets/js/translate.js
@@ -0,0 +1,101 @@
+
+
+$(document).ready(function(){
+  
+
+    $(document).click( function(e) {
+         $('.translate_wrapper, .more_lang').removeClass('active');
+    });
+  
+    $('.translate_wrapper .current_lang').click(function(e){
+      e.stopPropagation();
+      $(this).parent().toggleClass('active');
+  
+      setTimeout(function(){
+        $('.more_lang').toggleClass('active');
+      }, 5);
+    });
+    var lang = "en";
+      $(".lang").each(function(index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+        
+      });
+    document.title=titleLang[lang];
+    $("[data-placeholder]").each(function() {
+      var placeholder = $(this).attr("data-placeholder");
+      $(this).attr("placeholder", arrLang[lang][placeholder]);
+    });
+    /*TRANSLATE*/
+  
+  
+    $('.more_lang .lang').click(function(){
+      $(this).addClass('selected').siblings().removeClass('selected');
+      $('.more_lang').removeClass('active');
+  
+      var img = $(this).find('img').attr('src');
+      var lang = $(this).attr('data-value');
+    
+      
+  
+      $('.current_lang .lang-txt').text(lang);
+      $('.current_lang img').attr('src', img);
+  
+   
+      $(".translate").click(function() {
+        var lang = $(this).attr("id");
+        if(lang )
+        $(".lang").each(function(index, element) {
+          $(this).text(arrLang[lang][$(this).attr("key")]);
+        });
+        document.title=titleLang[lang];
+        $("[data-placeholder]").each(function() {
+          var placeholder = $(this).attr("data-placeholder");
+          $(this).attr("placeholder", arrLang[lang][placeholder]);
+        });
+      });
+    });
+  });
+  
+  
+    var titleLang ={
+      "en":"System Security 114 - Home",
+      "cn":"忘记密码 114 - 首頁",
+      "vn":"Hệ thống an toàn 114 - Trang chủ"
+    }
+    var arrLang ={
+      en:{
+        "inf": "Information",
+        "sys":"System Security 114",
+        "help": "Help",
+        "name":"Full name",
+        "email":"Email",
+        "submit":"Submit",
+        "string1":"Username : Card ID & Password : Last 6 digits of Identity Card"
+      },
+      cn:{
+        "inf":"首頁",
+        "sys":"忘记密码 114",
+        "help":"用戶名",
+        "name":"姓名",
+        "email":"電子郵件",
+        "submit":"確認",
+        "string1":"用户为工號初始密碼為身份證號碼后六位"
+      },
+      vn:{
+        "inf":"Thông tin",
+        "sys":"Hệ thống an toàn 114",
+        "help":"Trợ giúp",
+        "name":"Họ và tên",
+        "email":"Email",
+        "submit":"Xác nhận",
+        "string1":"Tên đăng nhập là mã thẻ, mật khẩu mặc định là sáu số cuối CMND"
+      }
+    }
+  
+  var tran=[
+    {
+      "en":"Username",
+      "cn":"阮公",
+      "user":"Tên đăng nhập"
+    }
+  ]
