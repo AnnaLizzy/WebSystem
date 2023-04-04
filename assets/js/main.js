b37377f9b73e072e404d91806927235a06f19c42
AnnaLizzy <thuthuha29@gmail.com>
2023-04-04 20:59:14 +0700

    sửa lại một chút

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,7 @@ jQuery(document).ready(
         var $ = jQuery.noConflict();
         $(document).ready(function($) {
             "use strict";
-            
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 500) {
-            $('.back-to-top').fadeIn();
-        } else {
-            $('.back-to-top').fadeOut();
-        }
-    });
-
+      
     // scroll body to 0px on click
     $('.back-to-top').click(function () {
         $('html, body').animate({
